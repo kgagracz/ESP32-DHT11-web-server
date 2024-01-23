3cd2cc63f7b15eb1756286c3a4d61a39b99a0d7c
Krystian Gagracz <krystian.gagracz@gmail.com>
2024-01-23 22:12:30 +0100

    added web server and smtp library with sending email

--- ESP32-DHT11-web-server.ino
+++ ESP32-DHT11-web-server.ino
@@ -1,72 +1,191 @@
+#include <Arduino.h>
+#include <ESP_Mail_Client.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 #include <DHT.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <ESP_Mail_Client.h>
 
+const String ssid = "SAMSUNG";
+const String password = "jednobuch";
+
+/** The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com */
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "esptemphum@gmail.com"
+#define AUTHOR_PASSWORD "uxlwvmqkxvjfjnon"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "krystian.gagracz@gmail.com"
+
+/* Declare the global used SMTPSession object for SMTP transport */
+SMTPSession smtp;
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
+//DHT
 #define DHTPIN 18
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+//DISPLAY
 #define OLED_SDA 21
 #define OLED_SCL 22
 #define WIRE Wire
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &WIRE);
 
+//LEDS
 #define LED_HUM_TOO_HIGH_PIN 2
 #define LED_TEMP_TOO_HIGH_PIN 4
 
-const String ssid = "TP-LINK_A137";
-const String password = "14521706";
-
+//MAX VALUES
 float const MAX_TEMPERATURE = 25.0;
 float const MAX_HUMIDITY = 50.0;
+WebServer server(80);
 
-float readDHTTemperature() {
-  // Sensor readings may also be up to 2 seconds
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  if (isnan(t)) {    
-    Serial.println("Failed to read from DHT sensor!");
-    return -1;
+void handleWiFi() {
+  IPAddress local_IP(192, 168, 31, 184);
+  IPAddress gateway(192, 168, 31, 31);
+  IPAddress subnet(255, 255, 0, 0);
+  //WiFi.config(local_IP, gateway, subnet);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
   }
-  else {
-    Serial.println(t);
-    return t;
-  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 }
 
-float readDHTHumidity() {
-  // Sensor readings may also be up to 2 seconds
-  float h = dht.readHumidity();
-  if (isnan(h)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return -1;
+void handleMailConfig(String alertMessage) {
+/*  Set the network reconnection option */
+  MailClient.networkReconnect(true);
+
+  /** Enable the debug via Serial port
+   * 0 for no debugging
+   * 1 for basic level debugging
+   *
+   * Debug port can be changed via ESP_MAIL_DEFAULT_DEBUG_PORT in ESP_Mail_FS.h
+   */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the Session_Config for user defined session credentials */
+  Session_Config config;
+
+  /* Set the session config */
+  config.server.host_name = SMTP_HOST;
+  config.server.port = SMTP_PORT;
+  config.login.email = AUTHOR_EMAIL;
+  config.login.password = AUTHOR_PASSWORD;
+  config.login.user_domain = "";
+
+  /*
+  Set the NTP config time
+  For times east of the Prime Meridian use 0-12
+  For times west of the Prime Meridian add 12 to the offset.
+  Ex. American/Denver GMT would be -6. 6 + 12 = 18
+  See https://en.wikipedia.org/wiki/Time_zone for a list of the GMT/UTC timezone offsets
+  */
+  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+  config.time.gmt_offset = 3;
+  config.time.day_light_offset = 0;
+
+   /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = F("ESP");
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = F("Przekroczono wartosci graniczne!");
+  message.addRecipient(F("Sara"), RECIPIENT_EMAIL);
+    
+  /*Send HTML message*/
+  /*String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP board</p></div>";
+  message.html.content = htmlMsg.c_str();
+  message.html.content = htmlMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;*/
+
+  //Send raw text message
+  String textMsg = alertMessage;
+  message.text.content = textMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* Connect to the server */
+  if (!smtp.connect(&config)){
+    ESP_MAIL_PRINTF("Connection error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
+    return;
   }
-  else {
-    Serial.println(h);
-    return h;
+
+  if (!smtp.isLoggedIn()){
+    Serial.println("\nNot yet logged in.");
   }
+  else{
+    if (smtp.isAuthenticated())
+      Serial.println("\nSuccessfully logged in.");
+    else
+      Serial.println("\nConnected with no Auth.");
+  }
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    ESP_MAIL_PRINTF("Error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
 }
 
-void setup() {
-  Serial.begin(115200);
 
-  //begining dht reading
-  dht.begin();
-  //beggining OLED display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
-  display.display();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
+void handleRoot() {
+  char msg[1500];
 
-  //setting pinmode for LED's
-  pinMode(LED_HUM_TOO_HIGH_PIN, OUTPUT);
-  pinMode(LED_TEMP_TOO_HIGH_PIN, OUTPUT);
-
-  //clearing display
-  delay(1000);
-  display.clearDisplay();
-  display.display();
+  snprintf(msg, 1500,
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='4'/>\
+    <meta name='viewport' content='width=device-width, initial-scale=1'>\
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>\
+    <title>ESP32 DHT Server</title>\
+    <style>\
+    html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center;}\
+    h2 { font-size: 3.0rem; }\
+    p { font-size: 3.0rem; }\
+    .units { font-size: 1.2rem; }\
+    .dht-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px;}\
+    </style>\
+  </head>\
+  <body>\
+      <h2>ESP32 DHT Server!</h2>\
+      <p>\
+        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>Temperatura</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&deg;C</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-tint' style='color:#00add6;'></i>\
+        <span class='dht-labels'>Wilgotnosc</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&percnt;</sup>\
+      </p>\
+  </body>\
+</html>",
+           readDHTTemperature(), readDHTHumidity()
+          );
+  server.send(200, "text/html", msg);
 }
 
 bool maxTempExceeded = false;
@@ -86,13 +205,112 @@ void handleDisplay(float t, float h) {
   display.display();
 }
 
-void loop() {
+float readDHTTemperature() {
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    return t;
+  }
+}
+
+float readDHTHumidity() {
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    return h;
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  handleWiFi();
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  dht.begin();
+  //beggining OLED display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+  display.display();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+
+  //setting pinmode for LED's
+  pinMode(LED_HUM_TOO_HIGH_PIN, OUTPUT);
+  pinMode(LED_TEMP_TOO_HIGH_PIN, OUTPUT);
+
+  //clearing display
+  delay(1000);
+  display.clearDisplay();
+  display.display();
+}
+
+bool messageSent = false;
+int secondsFromMessageSent = 0;
+void loop(){
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
   float t = readDHTTemperature();
   float h = readDHTHumidity();
   maxTempExceeded = t > MAX_TEMPERATURE;
   maxHumExceeded = h > MAX_HUMIDITY;
+  if (!messageSent && secondsFromMessageSent > 2 && (maxTempExceeded || maxHumExceeded)) {
+    String message = "Przekroczono wartosci graniczne! Temperatura: " + String(t) +"C, wilgotnosc: " + String(h) + "%." ;
+    handleMailConfig(message);
+    messageSent = true;
+    secondsFromMessageSent = 0;
+  }
   handleLEDs();
   handleDisplay(t, h);
   delay(1000);
+  secondsFromMessageSent++;
   display.clearDisplay();
+}
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    // ESP_MAIL_PRINTF used in the examples is for format printing via debug Serial port
+    // that works for all supported Arduino platform SDKs e.g. AVR, SAMD, ESP32 and ESP8266.
+    // In ESP8266 and ESP32, you can use Serial.printf directly.
+
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failed: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++)
+    {
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+
+      // In case, ESP32, ESP8266 and SAMD device, the timestamp get from result.timestamp should be valid if
+      // your device time was synched with NTP server.
+      // Other devices may show invalid timestamp as the device time was not set i.e. it will show Jan 1, 1970.
+      // You can call smtp.setSystemTime(xxx) to set device time manually. Where xxx is timestamp (seconds since Jan 1, 1970)
+      
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %s\n", MailClient.Time.getDateTimeString(result.timestamp, "%B %d, %Y %H:%M:%S").c_str());
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+    }
+    Serial.println("----------------\n");
+
+    // You need to clear sending result as the memory usage will grow up.
+    smtp.sendingResult.clear();
+  }
 }
